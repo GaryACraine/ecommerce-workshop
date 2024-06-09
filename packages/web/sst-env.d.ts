376f001f0cbb6e28301aca43e70030995f25d3d9
root <root@LAPTOP-BRQQK101>
2024-06-09 10:17:38 +0100

    deployed and tested

--- packages/web/sst-env.d.ts
+++ packages/web/sst-env.d.ts
@@ -3,17 +3,8 @@
 import "sst"
 declare module "sst" {
   export interface Resource {
-    Api: {
-      name: string
-      type: "sst.aws.Function"
-      url: string
-    }
     ApiRouter: {
       type: "sst.aws.Router"
-      url: string
-    }
-    WebApp: {
-      type: "sst.aws.Nextjs"
       url: string
     }
   }
